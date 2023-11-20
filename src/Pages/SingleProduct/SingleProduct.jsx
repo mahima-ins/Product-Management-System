1640aadfb28ca9e9e0e75d9d3d0b00fbf9f17888
mahima-ins <paudelmahima4@gmail.com>
2023-11-20 17:00:23 +0545

    modified product form css

--- src/Pages/SingleProduct/SingleProduct.jsx
+++ src/Pages/SingleProduct/SingleProduct.jsx
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { Navbar } from '../../Components/Navbar/Navbar'
 import './SingleProduct.css'
-import { useParams, } from 'react-router-dom'
+import { useNavigate, useParams, } from 'react-router-dom'
 import  axios  from 'axios'
 
 export const SingleProduct = () => {
+  const Navigate = useNavigate()
   
   const {id}=useParams()
   const[product, setProduct]=useState({})
+  //delete product
+  const DeleteProduct=async()=>{
+   const response= await axios.delete("https://654a248ee182221f8d529899.mockapi.io/products/" + id)
+   if(response.status==200){
+    Navigate('/')
+   }
+   else{
+    alert("something went wrong!! try again")
+   }
+  }
+  //fetch single product
   const fetchSingleProduct=async()=>{
    const response= await axios.get("https://654a248ee182221f8d529899.mockapi.io/products/" + id)
    setProduct(response.data)
@@ -16,7 +28,7 @@ export const SingleProduct = () => {
   fetchSingleProduct()
   },[])
   // console.log(product)
-  
+
 return (
     <>
     <Navbar/>
@@ -24,7 +36,8 @@ return (
       <img src={product.ProductImage}alt="Product Image"/> 
   <h2 className="product-name">{product.ProductName}</h2>
   <p className="product-description">{product.ProductDescription}</p>
- <mark> {product.ProductMaterial}</mark>
+ <mark> {product.ProductMaterial}</mark><br/>
+ <button onClick={DeleteProduct}>Delete</button>
 
       </div>   
    
